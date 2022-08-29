8b58dc4a7ab8de12f61a4e76b1eeac161bcedcd2
FulGr1m <o_zhedik@mail.ru>
2022-08-29 12:36:44 +0300

    start_files

--- oxana_2021/oxana_2021/views.py
+++ oxana_2021/oxana_2021/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def about(request):
-    return render(request,'about.html')
+    return render(request,'prog.html')
+
 
 def home(request):
     return HttpResponse('This is my home')
