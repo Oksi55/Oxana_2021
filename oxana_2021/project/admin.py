6368983482ddec66af878452f17148240470f03a
FulGr1m <o_zhedik@mail.ru>
2022-09-14 23:24:08 +0300

    start_files

--- oxana_2021/project/admin.py
+++ oxana_2021/project/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project,this_post
 
 # Register your models here.
 
 
 admin.site.register(Project)
+admin.site.register(this_post)
 
 
--- oxana_2021/project/models.py
+++ oxana_2021/project/models.py
@@ -2,7 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    title=models.CharField(max_length=100)
-    description = models.TextField()
-    technology = models.CharField(max_length=50)
-    image = models.FileField(upload_to='img/')
+    author = models.CharField(max_length=80)
+    body = models.TextField('Содержание', blank=True)
+    cteated_on = models.DateTimeField(auto_now=True)
+    post = models.ForeignKey('this_post', on_delete=models.CASCADE)
+
+class this_post(models.Model):
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
+
+
